383b9b9b67822246b74bfe3e4c3e630920a777a9
Golam Mursalin <golammursalin309@gmail.com>
2022-04-04 16:16:41 +0600

    header and routing added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+// Components
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
+// Routing
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,75 @@
+import React from "react";
+// Routing
+import { NavLink } from "react-router-dom";
+const Header = () => {
+  return (
+    <div>
+      <header className="text-gray-600 body-font  bg-green-500 ">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-2xl">Consumers Reports</span>
+          </div>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <NavLink
+              to="/home"
+              style={({ isActive }) => {
+                return {
+                  borderBottom: isActive ? "2px solid black" : "",
+                };
+              }}
+              className="mr-10 p-1 cursor-pointer text-black text-lg hover:text-gray-900 "
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/reviews"
+              style={({ isActive }) => {
+                return {
+                  borderBottom: isActive ? "2px solid black" : "",
+                };
+              }}
+              className="mr-10  p-1 cursor-pointer  text-black text-lg  hover:text-gray-900"
+            >
+              Reviews
+            </NavLink>
+            <NavLink
+              to="/dashboard"
+              style={({ isActive }) => {
+                return {
+                  borderBottom: isActive ? "2px solid black" : "",
+                };
+              }}
+              className="mr-10   p-1 cursor-pointer  text-black text-lg  hover:text-gray-900"
+            >
+              Dashboard
+            </NavLink>
+            <NavLink
+              to="/blogs"
+              style={({ isActive }) => {
+                return {
+                  borderBottom: isActive ? "2px solid black" : "",
+                };
+              }}
+              className="mr-10  p-1 cursor-pointer  text-black text-lg   hover:text-gray-900"
+            >
+              Blogs
+            </NavLink>
+            <NavLink
+              to="/about"
+              style={({ isActive }) => {
+                return {
+                  borderBottom: isActive ? "2px solid black" : "",
+                };
+              }}
+              className="mr-10   p-1 cursor-pointer  text-black text-lg  hover:text-gray-900"
+            >
+              About US
+            </NavLink>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
